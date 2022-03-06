bb5c0a3f2565fa7ebd5c988938e406054c2dc839
topurayhan007 <topurayhan007@gmail.com>
2022-03-06 18:43:51 +0600

    ongoing...

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-const navLinks = document.querySelectorAll('.nav-link')
-const menuToggle = document.getElementById('toggleMobileMenu')
-const bsCollapse = new bootstrap.Collapse(menuToggle)
-navLinks.forEach((l) => {
-    l.addEventListener('click', () => { bsCollapse.toggle() })
-})
+// import bootstrap from "bootstrap"
+
+// const navLinks = document.querySelectorAll('.nav-link')
+// const menuToggle = document.getElementById('toggleMobileMenu')
+// const bsCollapse = new bootstrap.collapse(menuToggle)
+// navLinks.forEach((l) => {
+//     l.addEventListener('click', () => { bsCollapse.toggle() })
+// })

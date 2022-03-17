e91d43f802e0302bddefccb583396a62b9a0544f
topurayhan007 <topurayhan007@gmail.com>
2022-03-17 23:28:24 +0600

    done !!!!!

---
+++ contact.php
@@ -0,0 +1,23 @@
+<?php
+    $message_sent = false;
+
+    if( isset($_POST['email']) && $_POST['email'] != '' ) {
+
+        if( filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ){
+
+            $email = $_POST['email'];
+            $message = $_POST['message'];
+
+            $my_email = "topurayhan007@gmail.com";
+            $mail = "";
+            $mail .= $email. "\r\n";
+            $mail .= $message. "\r\n";
+
+            mail($my_email, ,$message, $mail);
+
+            $message_sent = true;
+        }
+    }
+
+
+?>

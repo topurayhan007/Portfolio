f1f960b8c2c9ac42cd531641c02691a24bb858ad
topurayhan007 <topurayhan007@gmail.com>
2022-03-18 11:35:27 +0600

    trying to send emails from website

--- contact.php
+++ contact.php
@@ -1,20 +1,17 @@
 <?php
-    $message_sent = false;
-
+    
     if( isset($_POST['email']) && $_POST['email'] != '' ) {
 
         if( filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ){
 
             $email = $_POST['email'];
             $message = $_POST['message'];
-            $subject = "From website";
+            $subject = 'From website';
 
-            $my_email = "topurayhan007@gmail.com";
-            $mail = $email. "\r\n" . $message. "\r\n";
+            $to = 'topurayhan007@gmail.com';
+            $mail = '' . $email . "\r\n" . $message . "\r\n";
 
-            mail($my_email, $subject, $message, $mail);
-
-            $message_sent = true;
+            mail($to, $subject, $mail);
         }
     }
 

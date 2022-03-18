3eaa3d0588ef901ffa33a9705dcad39f772eb80c
topurayhan007 <topurayhan007@gmail.com>
2022-03-18 12:18:24 +0600

    trying to send emails from website

--- contact.php
+++ contact.php
@@ -6,10 +6,11 @@
             $message = $_POST['message'];
             $subject = 'From website';
 
-            $to = 'topurayhan007@gmail.com';
+            $to = '1930179@iub.edu.bd';
             $mail = '' . $email . "\r\n" . $message . "\r\n";
 
             mail($to, $subject, $mail);
+            header("Location: index.html");
             echo $thankYou="<p>Thank you! We will be in contact with you shortly.</p>";
         
     }
